5b523e1156c955b49bd36d8301885af9be0dbee5
Naufal Asha <55681676+NaufalAshaW@users.noreply.github.com>
2019-10-23 15:37:10 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118063/latihan44/TesterOhm.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118063.latihan44;
+
+/**
+ *
+ * @author
+ * NAMA     : Naufal Asha
+ * KELAS    : IF-2
+ * NIM      : 10118063
+ */
+public class TesterOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Baterai();
+        Baterai baterai = new Baterai(3,12);
+    }
+    
+}
